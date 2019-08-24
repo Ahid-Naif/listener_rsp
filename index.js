09c3f1fd804e240d16c6ea1f6be3bafc7b3ef8f7
unknown <ahedqaood19@gmail.com>
2019-08-25 01:30:28 +0300

    set baudRate at 9600

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/ttyS0');
+const port = new SerialPort('/dev/ttyS0', {baudRate: 9600});
 
 const buadRate = 9600;
 const delay_ms = 1000 / buadRate;
 
 port.on('data', (data) => {
-  // console.log(data.toString('hex'));
+  console.log(data.toString('hex'));
     
-  var counter = 0;
-  var interval = setInterval( (data) => {
-    console.log(data.toString('hex'));  
+  // var counter = 0;
+  // var interval = setInterval( (data) => {
+  //   console.log(data.toString('hex'));  
 
-    counter++;
-    if(counter == 14){
-      clearInterval(interval);
-    }
-  }
-    , delay_ms);
+  //   counter++;
+  //   if(counter == 14){
+  //     clearInterval(interval);
+  //   }
+  // }
+  //   , delay_ms);
 });
