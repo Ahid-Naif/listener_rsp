f02840e4c08a692fb2698dd325a7da6f5729ba13
unknown <ahedqaood19@gmail.com>
2019-08-25 01:52:02 +0300

    read data using parser

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const SerialPort = require('serialport');
-const port = new SerialPort('/dev/ttyS0', {baudRate: 9600});
+const SerialPort = require('serialport')
+const ByteLength = require('@serialport/parser-byte-length')
+const port = new SerialPort('/dev/ttyS0')
 
-const buadRate = 9600;
-const delay_ms = 1000 / buadRate;
-
-port.on('data', (data) => {
-  console.log(data.toString('hex'));
-    
-  // var counter = 0;
-  // var interval = setInterval( (data) => {
-  //   console.log(data.toString('hex'));  
-
-  //   counter++;
-  //   if(counter == 14){
-  //     clearInterval(interval);
-  //   }
-  // }
-  //   , delay_ms);
-});
+const parser = port.pipe(new ByteLength({length: 14}))
+parser.on('data', console.log) // will have 8 bytes per data event
