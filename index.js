35ed7d2942a88b0bf317951209e4dcdbf0871a61
unknown <ahedqaood19@gmail.com>
2019-08-25 02:06:48 +0300

    retrieve data in a String format

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  console.log(data);
-  console.log(data.toJSON());
+  full_message = data.toJSON().data;
+  console.log(full_message);
+  console.log(typeof(full_message));
 });
