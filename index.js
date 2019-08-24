46b2d8834dc29d94f98f710b4d3540098b84a157
unknown <ahedqaood19@gmail.com>
2019-08-25 02:35:42 +0300

    keep the id in HEX format

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  let id = data.toJSON().data;
+  let id = data.toJSON().data.toString(16);
   id.shift(); // remove 1st byte (start-byte)
   id.pop(); // remove last byte (end-byte)
   console.log(id);
