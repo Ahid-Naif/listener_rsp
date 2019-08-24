9eea7467e0611c55935b6e4c516eaaa34b073cb6
unknown <ahedqaood19@gmail.com>
2019-08-24 04:57:16 +0300

    use print function to see output

--- listener.py
+++ listener.py
@@ -4,5 +4,7 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 while True:
     port.write("\r\nSay something:")
+    print("hey")
     rcv = port.read(10)
+    print(rcv)
     port.write("\r\nYou sent:" + repr(rcv))
