b9fe4650c5bdc5a5755d5a0a3d7ba9057567eba4
unknown <ahedqaood19@gmail.com>
2019-08-24 19:57:37 +0300

    keep baudRate as the default

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const raspi = require('raspi');
 const Serial = require('raspi-serial').Serial;
  
 raspi.init(() => {
-  var serial = new Serial('/dev/ttyS0', { baudRate: 9600 });
+  var serial = new Serial('/dev/ttyS0');
   serial.open(() => {
     serial.on('data', (data) => {
       process.stdout.write(data);
