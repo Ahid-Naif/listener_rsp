8bac0de95174d75c0ce29e827cf75f4cee800023
unknown <ahedqaood19@gmail.com>
2019-08-25 01:27:52 +0300

    convert data into HEX

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const SerialPort = require('serialport');
 const port = new SerialPort('/dev/ttyS0');
 
+const buadRate = 9600;
+const delay_ms = 1000 / buadRate;
 
-  port.on('data', (data) => {
+port.on('data', (data) => {
+  // console.log(data.toString('hex'));
     
-    console.log(data.toString('hex'));
-  });
+  var counter = 0;
+  var interval = setInterval( (data) => {
+    console.log(data.toString('hex'));  
+
+    counter++;
+    if(counter == 14){
+      clearInterval(interval);
+    }
+  }
+    , delay_ms);
+});
