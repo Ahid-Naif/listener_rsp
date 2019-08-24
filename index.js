562fb25e76229fa132aba496745f2ea6791907eb
unknown <ahedqaood19@gmail.com>
2019-08-25 02:01:39 +0300

    convert buffer into JSON object

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const ByteLength = require('@serialport/parser-byte-length')
 const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
-parser.on('data', console.log) // will have 14 bytes per data event
+parser.on('data', (data) => { // will have 14 bytes per data event
+  console.log(data);
+  console.log(JSON.parse(data.toString()));
+});
