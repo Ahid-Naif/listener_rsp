ac0c2ce5d8bce0edad06bd6d0eb3f7e3d190845a
unknown <ahedqaood19@gmail.com>
2019-08-25 02:04:15 +0300

    try a different approach

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port = new SerialPort('/dev/ttyS0')
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
   console.log(data);
-  console.log(JSON.parse(data.toString()));
+  console.log(data.toJSON());
 });
