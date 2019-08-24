d387382b1b845a59d347f48ec3eb3f36a66c7f3c
unknown <ahedqaood19@gmail.com>
2019-08-25 01:56:51 +0300

    correct a typo in the comment

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const ByteLength = require('@serialport/parser-byte-length')
 const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
-parser.on('data', console.log) // will have 8 bytes per data event
+parser.on('data', console.log) // will have 14 bytes per data event
