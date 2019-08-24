689858c5542e655cf93a4b1f7ad08ece27de6baf
unknown <ahedqaood19@gmail.com>
2019-08-24 04:29:36 +0300

    correct a mistake

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
-import serial
+from serial import Serial
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+port = Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 while True:
     port.write("\r\nSay something:")
