426f167acff00fcdf67e2df319d73abae07601cc
unknown <ahedqaood19@gmail.com>
2019-08-25 02:19:02 +0300

    fix a bug

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  let full_message = data.toJSON().data;
-  let id = full_message.shift(); // remove 1st byte (start-byte)
-  id = id.pop(); // remove last byte (end-byte)
+  let id = data.toJSON().data;
+  id.shift(); // remove 1st byte (start-byte)
+  id.pop(); // remove last byte (end-byte)
   console.log(id);
 });
