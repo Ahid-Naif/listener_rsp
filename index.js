59991752a250f86711f8dbf95d63dd32cdc0dab2
unknown <ahedqaood19@gmail.com>
2019-08-24 23:13:12 +0300

    print type of data

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const SerialPort = require('serialport');
 const port = new SerialPort('/dev/ttyS0');
 
+const baudRate = 9600;
+const delay_ms = 1000 / baudRate;
 
-  port.on('data', (data) => {
-    
-    console.log(data);
-  });
+var counter = 0;
+port.on('data', (data) => {
+  console.log(typeof(data));
+  setInterval( () => {
+      
+    counter++;
+    if(counter == 14){
+      clearInterval();
+    }
+  }
+      
+  , delay_ms)
+});
  
 // open errors will be emitted as an error event 
 port.on('error', function(err) {
