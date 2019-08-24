e4ffc2338f8fe0097a295659b637977cc2c16919
unknown <ahedqaood19@gmail.com>
2019-08-24 22:54:16 +0300

    try a different approach

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const SerialPort = require('serialport');
 const port = new SerialPort('/dev/ttyS0');
 
-const baudRate = 9600;
-const delay_ms = 1000 / baudRate;
 
-port.on('open', () => {
   port.on('data', (data) => {
-    setInterval(function
-      
-      , delay_ms)
+    
     console.log(data);
   });
-});
  
 // open errors will be emitted as an error event 
 port.on('error', function(err) {
