1d6a875ca516ee98090c350cf2fa7839e3f33a9e
unknown <ahedqaood19@gmail.com>
2019-08-25 04:19:26 +0300

    re-factor the code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const SerialPort = require('serialport')
 const ByteLength = require('@serialport/parser-byte-length')
 const port = new SerialPort('/dev/ttyS0')
 
-const retrieve = require('./retrieveInfo.js')
+const getId = require('./retrieveInfo.js')
 
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  id = retrieve.getID(data);
+  id = getID(data);
   console.log(id);
 });
