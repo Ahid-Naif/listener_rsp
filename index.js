2259bd683adba4752886004ac0af21c9d92af823
unknown <ahedqaood19@gmail.com>
2019-08-25 03:10:58 +0300

    retrieve the id of the card from the whole received message

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const port = new SerialPort('/dev/ttyS0')
 
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  let id = data.toJSON().data;
-  id.shift(); // remove 1st byte (start-byte)
-  id.pop(); // remove last byte (end-byte)
-  for (i in id) {
-    console.log(id[i].toString(16))
-  } 
+  let full_message = data.toJSON().data;
+  let id = full_message.slice(7, 11);
   
+  console.log(id);
 });
