7ee4cb158c19f5cbb370009cb7e44bb88a6c7e41
unknown <ahedqaood19@gmail.com>
2019-08-25 03:20:19 +0300

    correct a mistake

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ parser.on('data', (data) => { // will have 14 bytes per data event
   for (i in id) {
     id_hex += id[i].toString(16);
   }
-  id_dec = id_hex.parserInt(16);
+  id_dec = id_hex.parserInt(10);
   console.log(id_dec);
 });
