7496b35a284f5e0013d4dcbbf9919a3b79799379
unknown <ahedqaood19@gmail.com>
2019-08-25 03:56:58 +0300

    only print the final result (card id in DEC format

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ parser.on('data', (data) => { // will have 14 bytes per data event
   for (i in id) {
     id_hex += id[i].toString(16);
   }
-  console.log(id_hex);
 
   let hex  = id_hex.toString();
 	let id_ascii = '';
