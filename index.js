f6a5c348d3d7510b53eb61283e0d8a08a8d51578
unknown <ahedqaood19@gmail.com>
2019-08-25 04:18:34 +0300

    re-factor the code

--- index.js
+++ index.js
@@ -2,22 +2,10 @@ const SerialPort = require('serialport')
 const ByteLength = require('@serialport/parser-byte-length')
 const port = new SerialPort('/dev/ttyS0')
 
+const retrieve = require('./retrieveInfo.js')
+
 const parser = port.pipe(new ByteLength({length: 14}))
 parser.on('data', (data) => { // will have 14 bytes per data event
-  let full_message = data.toJSON().data;
-  let id = full_message.slice(7, 11);
-  
-  let id_hex = "";
-  for (i in id) {
-    id_hex += id[i].toString(16);
-  }
-
-  let hex  = id_hex.toString();
-	let id_ascii = '';
-	for (let i = 0; i < hex.length; i += 2) {
-    id_ascii += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-  }
-
-  let id_dec = parseInt(id_ascii, 16).toString(10);
-  console.log(id_dec);
+  id = retrieve.getID(data);
+  console.log(id);
 });
---
+++ retrieveInfo.js
@@ -0,0 +1,22 @@
+function getID(data) {
+    let full_message = data.toJSON().data; // convert data into JSON object
+    let id = full_message.slice(7, 11); // get the id from the message
+  
+    let id_hex = ""; // to store id in HEX format
+    // concatenate the bytes into one string
+    for (i in id) {
+        id_hex += id[i].toString(16);
+    }
+
+    let id_ascii = ''; // to store id in ASCII format
+    // convert id from HEX into ASCII
+    for (let i = 0; i < hex.length; i += 2) {
+        id_ascii += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
+
+    // convert id from ASCII into DEC
+    let id_dec = parseInt(id_ascii, 16).toString(10);
+    return id_dec;
+};
+
+module.exports = getID;
