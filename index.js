36fce304f16e019073e19487863bfce2236a68df
unknown <ahedqaood19@gmail.com>
2019-08-25 03:43:40 +0300

    correct a mistake

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ parser.on('data', (data) => { // will have 14 bytes per data event
   let id_hex  = id.toString();
 	let id_ascii = '';
 	for (let i = 0; i < id_hex.length; i += 2) {
-	let id_ascii = '';
     id_ascii += String.fromCharCode(parseInt(id_hex.substr(i, 2), 16));
 	}
   console.log(id_ascii);
