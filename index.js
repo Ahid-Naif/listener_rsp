91d7fd2fc388c6cc00cb4b26cd17c12c4a2a6d71
unknown <ahedqaood19@gmail.com>
2019-08-25 05:01:49 +0300

    read card id only once

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const port = new SerialPort('/dev/ttyS0')
 const getId = require('./retrieveInfo.js')
 
 const parser = port.pipe(new ByteLength({length: 14}))
+let previousId = '';
 parser.on('data', (data) => { // will have 14 bytes per data event
+  if(previousId == id){
+    return;
+  }
   id = getId(data);
+  previousId = id
   console.log(id);
 });
