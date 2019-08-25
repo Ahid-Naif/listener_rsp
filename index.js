e29649b39a5e41f921a57169d8735a112b251b30
unknown <ahedqaood19@gmail.com>
2019-08-25 03:46:55 +0300

    fix a bug

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ parser.on('data', (data) => { // will have 14 bytes per data event
   let full_message = data.toJSON().data;
   let id = full_message.slice(7, 11);
   
-  let id_hex  = id.toString();
+  let id_hex = "";
+  for (i in id) {
+    id_hex += id[i].toString(16);
+  }
+  console.log(id_hex);
+
+  let hex  = id_hex.toString();
 	let id_ascii = '';
-	for (let i = 0; i < id_hex.length; i += 2) {
-    id_ascii += String.fromCharCode(parseInt(id_hex.substr(i, 2), 16));
-	}
+	for (let i = 0; i < hex.length; i += 2) {
+    id_ascii += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
   console.log(id_ascii);
-  console.log(typeof(id_ascii));
 });
